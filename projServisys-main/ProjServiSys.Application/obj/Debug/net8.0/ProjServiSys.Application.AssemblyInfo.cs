4dd8e55468a2b506a05759ff97296138e992d505
g4bh <gabriella266@gmail.com>
2024-06-09 19:40:35 -0300

    adicionado modal nas tabelas

--- projServisys-main/ProjServiSys.Application/obj/Debug/net8.0/ProjServiSys.Application.AssemblyInfo.cs
+++ projServisys-main/ProjServiSys.Application/obj/Debug/net8.0/ProjServiSys.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjServiSys.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ec934d3c2dc3bee8be9350653a916306941cca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c9677eef748debbdd82eae078dab33564e39da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjServiSys.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjServiSys.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
